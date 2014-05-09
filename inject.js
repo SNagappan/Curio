5697c67234722acfb0b8ca14a51702b5855e1de9
Guoxing Li <liguoxing64@gmail.com>
2014-05-08 20:01:08 -0700

    Fix the bug that injects html multiple times

--- inject.js
+++ inject.js
@@ -1,6 +1,17 @@
 console.log('loaded');
 var file = "/" + jsParams.type + ".html";
-$.get(chrome.extension.getURL(file), function(data) {
-  $(data).appendTo('body');
-});
-
+var exists = false;
+if (jsParams.type == "question") {
+  if ($("#curioQ").length > 0) {
+    exists = true;
+  }
+} else if (jsParams.type == "alert") {
+  if ($("#curioA").length > 0) {
+    exists = true;
+  }
+}
+if (!exists) {
+  $.get(chrome.extension.getURL(file), function(data) {
+    $(data).appendTo('body');
+  });
+}
